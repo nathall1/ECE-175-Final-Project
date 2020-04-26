88a6d00ce71b5df495f854c25d7d42e771fe6813
Wesley <wchiu622@gmail.com>
2020-04-26 05:40:08 -0700

    Add project files.

---
+++ Final Project.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+#include<stdbool.h>
+#include<stdlib.h>
+#include<string.h>
+
+// Card definition: Suit, card value, pointer to next node and previous node (double-linked list)
+typedef struct card_s {
+	char suit[9];
+	int value;
+	struct card_s* next, * previous;
+};
+
+// Function Definitions
+void createDeck()
+
+
+int main(void)
+{
+
+}
